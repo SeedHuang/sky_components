6e716d7741e8f14e0d250418cb0d4b645fccc61f
SeedHuang <worldtreekeeper@gmail.com>
2019-01-10 14:07:53 +0800

    docs: add console

--- src/nav/index.jsx
+++ src/nav/index.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-
+console.log('this is nav')
 export default class Nav extends PureComponent {
   render() {
     return <nav className="sky_nav" {...this.props} />
--- src/side/index.jsx
+++ src/side/index.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+console.log('this is side')
 export default class Side extends PureComponent {
   render() {
     return <nav className="sky_side" {...this.props} />
