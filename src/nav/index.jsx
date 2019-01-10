1a5e5995f5251d22d7a44c1adeee93cdd1057bc5
SeedHuang <worldtreekeeper@gmail.com>
2019-01-10 14:29:59 +0800

    doc update to 0.3

--- src/nav/index.jsx
+++ src/nav/index.jsx
@@ -1,9 +1,10 @@
 import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-console.log('this is nav')
+
 export default class Nav extends PureComponent {
   render() {
+    console.log('this is nav');
     return <nav className="sky_nav" {...this.props} />
   }
 };
--- src/side/index.jsx
+++ src/side/index.jsx
@@ -1,9 +1,10 @@
 import React, {PureComponent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-console.log('this is side')
+
 export default class Side extends PureComponent {
   render() {
+    console.log('this is side');
     return <nav className="sky_side" {...this.props} />
   }
 };
